a47bdd7c4ea38b715f0d589e946003865ea5590d
VitalVilarinho <vital.joao.vilarinho@gmail.com>
2020-04-30 17:21:51 +0100

    Merge done

--- Source/BoardGameTracker/BGT.Domain/Enums/GameResult.cs
+++ Source/BoardGameTracker/BGT.Domain/Enums/GameResult.cs
@@ -3,15 +3,8 @@
     public enum GameResult
     {
         Win,
-<<<<<<< HEAD
         Defeat,
         Tie,
         Canceled
     }
 }
-=======
-        Draw,
-        Lose
-    }
-}
->>>>>>> e37e5a003de59fe5f9723eb8cc610982c530ca9a
